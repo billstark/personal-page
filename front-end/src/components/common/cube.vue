14b764ac2330dc6cd5060c8b356e0980a3d8a06e
Yang Zhuohan <billstark1996@gmail.com>
2017-06-01 22:44:47 +0800

    add small cube template

---
+++ front-end/src/components/common/cube.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="cube">
+        <section class="container">
+            <div class="cube-content">
+                <figure class="front"></figure>
+                <figure class="back"></figure>
+                <figure class="right"></figure>
+                <figure class="left"></figure>
+                <figure class="top"></figure>
+                <figure class="bottom"></figure>
+            </div>
+        </section>
+    </div>
+</template>
