1be518cfc1e20c2be599187f54d1ca300e8b8814
Yang Zhuohan <billstark1996@gmail.com>
2018-01-27 17:31:59 +0800

    finish categories api

--- api/app/controllers/gallery/categories_controller.rb
+++ api/app/controllers/gallery/categories_controller.rb
@@ -1,17 +1,66 @@
 module Gallery
   class CategoriesController < ApplicationController
+
+    include Response
     
-    before_action :sync, only: [index]
+    before_action :sync, only: [:index]
     
     def index
       @categories = Category.all
+      response = Array.new
+      @categories.each { |category|
+        response.push({ "id" => category["id"], "cname" => category["cname"] })
+      }
+      json_response(response)
     end
 
-    private
-      
+    ###################
+    # private methods #
+    ###################
+    private 
+
+    # Syncs all the images from cloud
     def sync
-      
+      categories = getCategories()
+      syncData(categories)
     end
 
+    # Get all categories in the image server
+    def getCategories
+      folders = Cloudinary::Api.root_folders["folders"]
+      categories = Array.new
+      folders.each { |folder|
+        categories.push(folder["name"])
+      }
+
+      return categories
+    end
+
+    # Sync image server data with my server data
+    def syncData(categories)
+      addMissingCategory(categories)
+      removeDeletedCategory(categories)
+    end
+
+    # Add missing Categories
+    def addMissingCategory(categories)
+      categories.each { |category| 
+        unless !Category.find_by(cname: category) then
+          next
+        end
+        Category.create(cname: category)
+      }
+    end
+
+    # remove deleted categories
+    def removeDeletedCategory(categories)
+      currentCategories = Category.all
+      currentCategories.each { |category| 
+        unless !categories.include?(category.cname) then
+          next
+        end
+        category.destroy
+      }
+    end
   end
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
       resources :images
     end
   end
+  
 end
